427f3d532cd7e9a10596cc9d1d7cc666abef9672
Yeeted-Potato <evanmhewett@gmail.com>
2026-07-31 13:14:08 +1200

    Added customer id

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+@app.get("/customers/{customer_id}")
+def get_customer(customer_id: int):
+    return {"You asked for customer": customer_id}
